570fcc1cbc07f60787a5abda875159629f067137
Abdelrahmanghoniem <116071431+Abdelrahmanghoniem@users.noreply.github.com>
2024-11-10 06:30:54 +0200

    fixed the css background colour problem and removed index.css , added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from "./components/navbar"
 import HomePage from './pages/HomePage'
+import { Container } from '@mui/material';
 
 function App() {
 
   return (
     <BrowserRouter>
+      <div className="custom-bg">
+
     <Navbar />
     
+    <Container className="my-5">
+
     <Routes>
       <Route path="/" element={<HomePage />} />
     </Routes>
+    </Container>
+    </div>
+    
+    
+
   </BrowserRouter>
 
 
